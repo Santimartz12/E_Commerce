95f6f4d574deb143eb0359b2a65550425e2b4803
Santiago <santimartz12@gmail.com>
2022-07-16 15:24:09 -0500

    Validacion de formulario LogIn

---
+++ js/aluraGeekUser.js
@@ -0,0 +1,38 @@
+//Si es la primera vez que se abre la pagina web se creará la informacion que el usuario no se ha registrado
+if(!sessionStorage.getItem("Login")){
+    let loginData = {user: "client", logged: false}
+    sessionStorage.setItem("Login",JSON.stringify(loginData));
+}
+
+//Llamamos los datos para determinar el estado del usuario
+let loggedData = JSON.parse(sessionStorage.getItem("Login"))
+
+
+//Creamos el entorno para el usuario registrado
+if(loggedData.logged && loggedData.user == "client" ){
+
+    //Eliminamos el boton
+    const botonlogin = document.querySelector("[data-btn-login]");
+    botonlogin.parentNode.innerHTML = "";
+
+    //Agregamos una imagen para reemplazar el boton
+    const header = document.querySelector("[data-container-header]")
+    const img = document.createElement("img");
+    img.classList.add("header__perfil")
+
+    //Link de la imagen de perfil
+    img.src = "https://static9.depositphotos.com/1594920/1087/i/600/depositphotos_10879124-stock-photo-young-chimpanzee-simia-troglodytes-6.jpg"
+
+    header.appendChild(img);
+}
+
+//Creamos el entorno para el admin en modo VISTA CLIENTE
+if(loggedData.logged && loggedData.user == "admin" ){
+
+    //Editamos el boton
+    const botonlogin = document.querySelector("[data-btn-login]");
+    const link = botonlogin.parentNode;
+    link.pathname = "./html/admin_page.html"
+    botonlogin.textContent = "Modo Admin";
+
+}
---
+++ js/login/login.js
@@ -0,0 +1,58 @@
+const formularioLogin = document.querySelector("[data-formulario-login]")
+
+formularioLogin.addEventListener("submit",(evento) => {
+    evento.preventDefault()
+
+    const correo = document.querySelector("[data-email]")
+    const password = document.querySelector("[data-password]")
+
+    comprobarlogin(correo,password);
+})
+
+
+const comprobarlogin = (correo,password) => {
+
+    // Llamamos a la lista de errores para ser mostrada si hay datos mal diligenciados
+    const error = document.querySelector("[data-error]");
+
+    //Si el correo de Admin y la contraseña de admin son iguales
+    if(correo.value.includes("@aluraadmin.com") && password.value == "Admin123"){
+
+        //Actualizamos los datos del local Storage
+        let loggedData = JSON.parse(sessionStorage.getItem("Login"))
+        loggedData.user = "admin";
+        loggedData.logged = true;
+        sessionStorage.setItem("Login",JSON.stringify(loggedData))
+
+        //Enviamos al usuario a la seccion del administrador
+        window.location.href = "../html/admin_page.html"
+
+    }
+    else{
+        //Si el correo es valido pero la contraseña es incorrecta
+        if(correo.value.includes("@aluraadmin.com") && password.value != "Admin123")
+        {
+            error.classList.remove("ocultar");
+        }
+    }
+
+    //Si el correo no es igual al del admin y la contraseña es correcta
+    if(!correo.value.includes("@aluraadmin.com") && password.value == "Alura123"){
+
+        //Actualizamos los datos del local Storage
+        let loggedData = JSON.parse(sessionStorage.getItem("Login"))
+        loggedData.user = "client";
+        loggedData.logged = true;
+        sessionStorage.setItem("Login",JSON.stringify(loggedData))
+        
+        //Enviamos al usuario a la seccion del administrador
+        window.location.href = "../index.html"
+    }
+    else{
+        //Si la contraseña es incorrecta pero el correo no es igual al del admin
+        if(!correo.value.includes("@aluraadmin.com") && password.value != "Alura123")
+        {
+            error.classList.remove("ocultar");
+        }
+    }
+}
